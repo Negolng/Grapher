831fcf5e2117127286414a6b0993914afc98d211
Negolng <114674976+Negolng@users.noreply.github.com>
2023-10-28 13:35:00 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,48 @@
 import sys
-import matplotlib
-from PyQt5 import QtCore, QtWidgets
-
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5 import uic
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
-
-matplotlib.use('Qt5Agg')
-
-
-class MplCanvas(FigureCanvasQTAgg):
-
-    def __init__(self, parent=None, width=5, height=4, dpi=100):
-        fig = Figure(figsize=(width, height), dpi=dpi)
-        self.axes = fig.add_subplot(111)
-        super(MplCanvas, self).__init__(fig)
+import random
 
 
-class MainWindow(QtWidgets.QMainWindow):
-
+class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        uic.loadUi('gui.ui', self)
 
-        # Create the maptlotlib FigureCanvas object,
-        # which defines a single set of axes as self.axes.
-        sc = MplCanvas(self, width=5, height=4, dpi=100)
-        sc.axes.plot([0, 1, 2, 3, 4], [10, 1, 20, 3, 40])
-        self.setCentralWidget(sc)
+        self.graph = Plot(self)
+        self.graph.move(200, 35)
+
+        self.randomButton.clicked.connect(self.plot_data)
+        self.clearButton.clicked.connect(self.clear_plot)
+
+        self.show()
+
+    def plot_data(self):
+        data = [random.random() for i in range(1, 10)]
+
+        self.graph.ax.plot(data, 'r-')
+        self.graph.draw()
+
+    def clear_plot(self):
+        pass
+    # ДОДЕЛАТЬ ОТЧИСТКУ ВСЕГО ЭТОГО ГОВНА БЛЯТЬ
+    # https://pythonspot.com/pyqt5-matplotlib/
+    # https://www.pythonguis.com/tutorials/plotting-matplotlib/
+    # https://matplotlib.org/stable/
+
+
+class Plot(FigureCanvas):
+    def __init__(self, parent=None, width=5, height=5, dpi=100):
+        self.fig = Figure(figsize=(width, height), dpi=dpi)
+        super().__init__(self.fig)
+        self.setParent(parent)
+        self.ax = self.figure.add_subplot(111)
+        self.ax.set_title('Kurwa')
 
 
 
-app = QtWidgets.QApplication(sys.argv)
-w = MainWindow()
-w.show()
-app.exec_()
+application = QApplication(sys.argv)
+window = MainWindow()
+sys.exit(application.exec())
