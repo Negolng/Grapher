9bb072e4b49b57d69303df9a76ff842756e17e35
Negolng <mahetov.pavel@gmail.com>
2023-11-08 14:42:14 +0500

    Added new files
    Not stable. НАДО ПОЧИНИТЬ, БЛЯТЬ

--- main.py
+++ main.py
@@ -1,86 +1,68 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QGridLayout, QLabel
 from PyQt5 import uic
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
 from math import floor
+from datetime import datetime
 
 # https://pythonspot.com/pyqt5-matplotlib/
 # https://www.pythonguis.com/tutorials/plotting-matplotlib/
 # https://matplotlib.org/stable/
 
 
-
-OPERANDS = {'/', '+', '-', '^', '*'}
+OPERANDS = '/*-+^'
+NUMBERS = '123456789'
+SPECIALS = OPERANDS + NUMBERS
 
 
 class FormulaError(Exception):
     pass
 
 
-class MathFunc:
+class LogWindow(QMainWindow):
     def __init__(self):
-        self.things_to_do = []
+        super().__init__()
+        uic.loadUi('log_gui.ui')
+        layout = QGridLayout()
+        for i in range(10):
+            label = QLabel(self)
+            label.setText(i)
+            print('!!!')
+            layout.addWidget(label)
+            print('a')
+        log_widget = QWidget()
+        log_widget.setLayout(layout)
+        self.logArea.setWidgt(log_widget)
 
-    @staticmethod
-    def multiply(x, y):
-        return x * y
 
-    @staticmethod
-    def divide(x, y):
-        return x / y
 
-    @staticmethod
-    def add(x, y):
-        return x + y
-
-    @staticmethod
-    def sub(x, y):
-        return x - y
-
-    @staticmethod
-    def pow(x, y):
-        return x**y
-
-    @staticmethod
-    def root(x, y):
-        return x**(1 / y)
-
-    def add_f(self, function):
-        self.things_to_do.append(function)
+def is_it_safe(line):
+    safe = True
+    if not ('y' in line or 'f(x)' in line or 'x' in line):
+        safe = False
+    return safe
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('gui.ui', self)
+        uic.loadUi('main_gui.ui', self)
 
         self.graph = Plot(self)
         self.graph.move(200, 35)
 
         self.plotButton.clicked.connect(self.calc_and_plot)
         self.clearButton.clicked.connect(self.clear_plot)
+        self.logButton.clicked.connect(self.show_logs)
         self.calc_and_plot()
         self.show()
 
     def function_interpreter(self):
         line = self.functionInput.text().lower()
-        func = MathFunc()
-
         left_part, right_part = line.split('=')
-        if 'y' in left_part or 'f(x)' in left_part or 'x' in left_part:
-            for i, symbol in enumerate(right_part):
-                if symbol == ' ':
-                    continue
-                if symbol == 'x':
-                    operand = right_part[i]
-                    while operand not in OPERANDS:
-                        i += 1
-                        operand = right_part[i]
-                    
-
-        else:
-            raise FormulaError
+        if is_it_safe(left_part):
+            return right_part.replace('^', '**')
 
     def calc_and_plot(self):
         func = lambda x: eval(self.function_interpreter())
@@ -102,8 +84,12 @@ class MainWindow(QMainWindow):
                 xdata.append(start)
                 start += step
             self.statusLabel.setText('Success')
+            with open('logs.txt', 'a') as f:
+                f.write(f'[{datetime.now()}] Calculated data Successfully!\n')
         except ValueError:
             xdata, ydata = None, None
+            with open('logs.txt', 'a') as f:
+                f.write(f'[{datetime.now()}] Calculation error\n')
             self.statusLabel.setText('Error')
         return xdata, ydata
 
@@ -115,6 +101,10 @@ class MainWindow(QMainWindow):
         self.graph.ax.cla()
         self.graph.draw()
 
+    def show_logs(self):
+        self.log_window = LogWindow()
+        self.log_window.show()
+
 
 class Plot(FigureCanvas):
     def __init__(self, parent=None, width=5, height=5, dpi=100):
@@ -122,7 +112,6 @@ class Plot(FigureCanvas):
         super().__init__(self.fig)
         self.ax = self.figure.add_subplot(111)
         self.ax.grid(True)
-        self.ax.set_title('Kurwa')
         self.setParent(parent)
 
 
