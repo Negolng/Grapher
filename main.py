627cce0cb30d9aa232e68810f593cb1eb30418c8
Negolng <mahetov.pavel@gmail.com>
2023-11-09 17:33:25 +0500

    Added new files and features
    Stable

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox, QInputDialog
 from PyQt5 import uic
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
 from math import floor
 import math as mt
 from datetime import datetime
-
-
+import sqlite3
 # КТО ПРОЕКТ ТРОНЕТ - В ЖОПУ ВЫЕБУ
 # Я серьёзно.
-
 OPERANDS = '/*-+^ '
 NUMBERS = '123456789'
 KEY_WORDS = {'sin', 'tan', 'mod', 'cos'}
@@ -38,34 +36,76 @@ def write_log(log):
         f.write(f'[{datetime.now()}] {log}\n')
 
 
+class LoadWindow(QMainWindow):
+    def __init__(self, parent):
+        super().__init__()
+        self.main_w = parent
+        uic.loadUi('load_gui.ui', self)
+        self.loadButton.clicked.connect(self.load_by_name)
+        self.deleteButton.clicked.connect(self.delete)
+        self.updateButton.clicked.connect(self.get_list)
+        self.get_list()
+
+    def get_list(self):
+        self.listWidget.clear()
+        connection = sqlite3.connect('database.sqlite')
+        cursor = connection.cursor()
+        request = '''
+                        SELECT * FROM functions
+                    '''
+        new_list = []
+        result = cursor.execute(request).fetchall()
+        connection.close()
+        for line in result:
+            number, name, formula = line
+            final_string = f'№{number} | Name: {name}, Formula: {formula}'
+            new_list.append(final_string)
+        self.listWidget.addItems(new_list)
+
+    def load_by_name(self):
+        name = self.nameInput.text()
+        if name:
+            connection = sqlite3.connect('database.sqlite')
+            cursor = connection.cursor()
+            names = set(map(''.join, cursor.execute('SELECT name FROM functions').fetchall()))
+            if name in names:
+                request = f'''
+                SELECT formula FROM functions
+                WHERE name = "{name}"
+                '''
+                formula = str(''.join(*cursor.execute(request).fetchall()))
+                connection.close()
+                self.main_w.functionInput.setText(formula)
+                self.hide()
+                self.nameInput.clear()
+            else:
+                msg = QMessageBox(self)
+                msg.setText("There is no such name in the list")
+                msg.setWindowTitle('Error')
+                msg.exec()
+
+    def delete(self):
+        connection = sqlite3.connect('database.sqlite')
+        cursor = connection.cursor()
+        request = f'''
+                DELETE FROM functions
+                WHERE name = "{self.nameInput.text()}"
+                '''
+        cursor.execute(request).fetchall()
+        connection.commit()
+        connection.close()
+        msg = QMessageBox(self)
+        msg.setText("Deleted successfully")
+        msg.setWindowTitle('Delete')
+        msg.exec()
+        self.get_list()
+        self.nameInput.clear()
+
+
 def is_it_safe(line):
     one = 'y' in line or 'f(x)' in line or 'x' in line
     two = all([symbol in ALLOWED for symbol in line])
     return one or two
-
-
-class LogWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        uic.loadUi('log_gui.ui', self)
-        self.updateButton.clicked.connect(self.read_logs)
-        self.clearButton.clicked.connect(self.remove_logs)
-        self.read_logs()
-        write_log('Logs window started successfully')
-
-    def read_logs(self):
-        self.logArea.clear()
-        logs = open('logs.txt', 'r')
-        self.logArea.addItems(logs.readlines())
-        logs.close()
-
-    def remove_logs(self):
-        with open('logs.txt', 'w') as f:
-            f.write('')
-        self.read_logs()
-        msg = QMessageBox(self)
-        msg.setText('Logs have been removed')
-        msg.show()
 
 
 class MainWindow(QMainWindow):
@@ -77,19 +117,24 @@ class MainWindow(QMainWindow):
         self.graph.move(200, 35)
 
         self.log_window = LogWindow()
+        self.load_window = LoadWindow(self)
 
         self.plotButton.clicked.connect(self.calc_and_plot)
         self.clearButton.clicked.connect(self.clear_plot)
         self.logButton.clicked.connect(self.log_window.show)
+        self.saveButton.clicked.connect(self.save_or_update)
+        self.loadButton.clicked.connect(self.load_window.show)
+
         self.calc_and_plot()
         write_log('Application started successfully')
         self.show()
 
     def function_interpreter(self):
         line = self.functionInput.text().lower()
-        left_part, right_part = line.split('=')
-        if is_it_safe(right_part):
-            return right_part.replace('^', '**')
+        if '=' in line:
+            left_part, right_part = line.split('=')
+            if is_it_safe(right_part):
+                return right_part.replace('^', '**')
         write_log('Error with function interpretation')
         self.statusLabel.setText('Error')
 
@@ -136,6 +181,45 @@ class MainWindow(QMainWindow):
         self.graph.draw()
         write_log('Cleared plot successfully')
 
+    def save_or_update(self):
+        connection = sqlite3.connect('database.sqlite')
+        cursor = connection.cursor()
+        names = set(map(''.join, cursor.execute('SELECT name FROM functions').fetchall()))
+        print(names)
+        name, ok_pressed = QInputDialog.getText(self, "Name input",
+                                                "Choose function's name")
+        if ok_pressed:
+            if name in names:
+                msg = QMessageBox(self)
+                msg.setText("This function exists already, it will be updated")
+                msg.setWindowTitle('Update')
+                msg.exec()
+                self.update_old_function(name)
+            else:
+                self.save_new_funtion(name)
+
+    def update_old_function(self, name):
+        connection = sqlite3.connect('database.sqlite')
+        cursor = connection.cursor()
+        request = f'''
+                    UPDATE functions
+                    SET formula = "{self.functionInput.text()}"
+                    WHERE name = "{name}"
+                 '''
+        cursor.execute(request).fetchall()
+        connection.commit()
+        connection.close()
+
+    def save_new_funtion(self, name):
+        connection = sqlite3.connect('database.sqlite')
+        cursor = connection.cursor()
+        request = f'''
+            INSERT INTO functions(name, formula) VALUES("{name}", "{self.functionInput.text()}")
+         '''
+        cursor.execute(request).fetchall()
+        connection.commit()
+        connection.close()
+
 
 class Plot(FigureCanvas):
     def __init__(self, parent=None, width=5, height=5, dpi=100):
@@ -144,6 +228,30 @@ class Plot(FigureCanvas):
         self.ax = self.figure.add_subplot(111)
         self.ax.grid(True)
         self.setParent(parent)
+
+
+class LogWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('log_gui.ui', self)
+        self.updateButton.clicked.connect(self.read_logs)
+        self.clearButton.clicked.connect(self.remove_logs)
+        self.read_logs()
+        write_log('Logs window started successfully')
+
+    def read_logs(self):
+        self.logArea.clear()
+        logs = open('logs.txt', 'r')
+        self.logArea.addItems(logs.readlines())
+        logs.close()
+
+    def remove_logs(self):
+        with open('logs.txt', 'w') as f:
+            f.write('')
+        self.read_logs()
+        msg = QMessageBox(self)
+        msg.setText('Logs have been removed')
+        msg.show()
 
 
 application = QApplication(sys.argv)
